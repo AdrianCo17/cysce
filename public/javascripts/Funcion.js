f87e0b6b9ad8d952fe2a231058055f8fb87cd902
AdrianCo17 <123600209+AdrianCo17@users.noreply.github.com>
2023-07-26 09:12:01 -0500

    Update Funcion.js

--- public/javascripts/Funcion.js
+++ public/javascripts/Funcion.js
@@ -12,7 +12,21 @@ fetch('/compra', {
   .catch(error => {
     console.error('Error during purchase:', error);
   });
-});
+
+  fetch('/venta', {
+    method: 'POST',
+    body: JSON.stringify(compraData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    window.location.href = '/';
+  })
+  .catch(error => {
+    console.error('Error during purchase:', error);
+  });
 
 function cerrarSesion() {
   // Hacer una petición al servidor para cerrar la sesión
